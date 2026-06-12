156ec4f8825259bcd8b9124d955dc5598a6e6ab4
Samuel Pelc <pelcsamuel464@gmail.com>
2026-06-12 11:42:45 +0200

    fix

--- projects/TWINAI/sw.js
+++ projects/TWINAI/sw.js
@@ -3,14 +3,18 @@ const ASSETS = [
   './',
   './index.html',
   './app.js',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return Promise.all(
+        ASSETS.map((url) =>
+          cache.add(url).catch((err) => console.warn('Cache add failed:', url, err))
+        )
+      );
+    })
   );
   self.skipWaiting();
 });
